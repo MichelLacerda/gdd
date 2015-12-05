6c5aeb31a33bbbca2e6d4aa19926474d68d0bacc
Michel Lacerda <michellacerdasilva@gmail.com>
2015-12-05 19:39:51 -0200

    Ajusta JS de Embed de Vídeo

--- static/js/youtube.js
+++ static/js/youtube.js
@@ -1,13 +1,13 @@
 $( document ).ready( function( ) {
   (function(data){
-    function embed( id ) {
+    var embed = (function ( id ) {
       var begin_div = '<div class="embed-responsive embed-responsive-16by9">';
       var begin_iframe = '<iframe width="854" height="480" src="https://www.youtube.com/embed/' + id + '" frameborder="0" allowfullscreen>';
       var end_iframe = '</iframe>';
       var end_div = '</div>';
       var string = '';
       return string.concat( begin_div , begin_iframe , end_iframe , end_div );
-    }
+    });
     
     if ( data ) {
       data.replaceWith( embed( data.attr( 'data' ) ) );
