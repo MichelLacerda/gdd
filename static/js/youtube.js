616f921c662ffb0c11bdca207b5ee6a9d284f2e1
Michel Lacerda <michellacerdasilva@gmail.com>
2015-12-09 23:08:47 -0200

    Ajuste de layout

--- static/js/youtube.js
+++ static/js/youtube.js
@@ -1,4 +1,4 @@
-$( document ).ready( function( ) {
+/*$( document ).ready( function( ) {
   (function( data ){
     var embed = (function ( id ) {
       var begin_div = '<div class="embed-responsive embed-responsive-16by9">';
@@ -16,3 +16,27 @@ $( document ).ready( function( ) {
     }
   })( $( 'em[id="youtube"]' ) );
 });
+
+*/
+
+// Load the IFrame Player API code asynchronously.
+var tag = document.createElement('script');
+tag.src = "https://www.youtube.com/player_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+// Replace the 'ytplayer' element with an <iframe> and
+// YouTube player after the API code downloads.
+var player;
+function onYouTubePlayerAPIReady() {
+  player = new YT.Player('ytplayer', {
+    /*height: '390',
+    width: '640',*/
+    videoId: 'M7lc1UVf-VE',
+    playerVars: {
+      'autoplay': 0,
+      'controls': 2,
+      'rel': 1,
+    }
+  });
+}
